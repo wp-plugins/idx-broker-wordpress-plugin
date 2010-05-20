5e54c8cb0fe30b90361755c805e086b545c36d98
idxco <idxco@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-05-20 18:06:20 +0000

    git-svn-id: https://plugins.svn.wordpress.org/idx-broker-wordpress-plugin/tags/1.2.0.0@243493 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- idxBroker.js
+++ idxBroker.js
@@ -2,8 +2,8 @@ jQuery(document).ready(function(){
     
     // when the save changes button is clicked
     
-    ajax_load = "<img src='/images/ajax-loader.gif' />";
-    ajaxPath = jQuery('#saveChanges').attr('ajax');
+    var ajax_load = "<img src='/images/ajax-loader.gif' />";
+    var ajaxPath = jQuery('#saveChanges').attr('ajax');
     
     jQuery('#saveChanges').click(function(event){
 
@@ -82,5 +82,6 @@ jQuery(document).ready(function(){
                 jQuery('#status').html('Updated Wrapper...').fadeOut('slow');
             }
         );
-    }); 
+    });
+    
 });
